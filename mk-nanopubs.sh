0f34e9ed167469e2b436b5bb78d413fa79af4256
Tobias Kuhn <kuhntobias@gmail.com>
2018-04-04 09:48:40 +0200

    Output nanopub counts

--- mk-nanopubs.sh
+++ mk-nanopubs.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 ./elton.sh nanopubs $1/$2 | sort -u | gzip > nanopubs/$1_$2_nanopubs.trig.gz
+
+COUNT=`cat nanopubs/$1_$2_nanopubs.trig.gz | gunzip | wc -l`
+echo "$COUNT nanopubs generated for $1/$2"
